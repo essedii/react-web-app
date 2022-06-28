476c6417d5f085b05920cc161f38653c8bc7b059
Stefano Dolci <ste.dolci16@gmail.com>
2022-06-28 16:46:07 +0200

    Update post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,23 +2,50 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const Posts = () => {
+
+    const[randomUserDataJSON, setRandomUserDataJSON] = useState('')
+    const[userInfos, setUserInfos] = useState([])
+
+    const fetchRandomData = () => {
+        return axios.get('https://randomuser.me/api')
+            .then( ({data}) => {
+                return (data)
+            })
+            .catch(err => {
+            console.log(err);
+            })
+
+    }
+
+    const getFullUserName = (userInfo) => {
+        const {name: {first, last}} = userInfo;
+        console.log()
+        return `${first} ${last}`;
+    }
+
+    useEffect(() => {
+                fetchRandomData().then(randomData => {
+                    setRandomUserDataJSON(JSON.stringify(randomData, null, 2) || 'No user found');
+                    setUserInfos(randomData.results);
+                })
+            },[])
+
+    return (
+        <div>
+            {
+                userInfos.map((userInfo) => {
+                    return <pre>{getFullUserName(userInfo)}</pre>
+        
+            
+        })}
+        {/* <pre>{randomUserDataJSON}</pre> */}
+        </div>
+       
+    )
 }
 
 
-    // axios.get('/user', {
-    //     params: {
-    //       ID: 12345
-    //     }
-    //   })
-    //   .then(function (response) {
-    //     console.log(response);
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   })
-    //   .then(function () {
-    //     // always executed
-    //   });  
+
 
 
 
