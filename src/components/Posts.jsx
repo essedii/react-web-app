b9ff547d41b018e411c7a6fa41a80c714d1dda4a
Stefano Dolci <ste.dolci16@gmail.com>
2022-06-28 13:16:59 +0200

    1

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,34 +2,60 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const Posts = () => {
-    const[randomUserDataJSON, setRandomUserDataJSON] = useState([])
-
-    const fectchRandomData = () => {
-        return axios.get('https://randomuser.me/api/')
-            .then((res) => {
-                console.log(data);
-                return JSON.stringify(res.data);
-        })
-            .catch(err => {
-                console.log(err)
-        })
-    }
-
-    useEffect(() => {
-        fectchRandomData().then(randomData => {
-            setRandomUserDataJSON(randomData)
-        })
-    })
-  return (
-    <div>
-            <ul>
-        {randomUserDataJSON.map(userDataJSON => {
-            <li key={userDataJSON.id}>{userDataJSON.title}</li>
-        })}
-    </ul>
-    </div>
-
-  )
 }
+
+
+    // axios.get('/user', {
+    //     params: {
+    //       ID: 12345
+    //     }
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   })
+    //   .then(function () {
+    //     // always executed
+    //   });  
+
+
+
+
+
+//     const[randomUserDataJSON, setRandomUserDataJSON] = useState()
+
+
+
+
+//     const fectchRandomData = () => {
+//         return axios.get('https://randomuser.me/api/')
+//             .then((res) => {
+//                 console.log(res.data)
+//                 return JSON.stringify(res.data);
+//         })
+//             .catch(err => {
+//                 console.log(err)
+//         })
+//     }
+
+//     useEffect(() => {
+//         fectchRandomData().then(randomData => {
+//             setRandomUserDataJSON(randomData)
+//         })
+//     })
+//   return (
+//     <div>
+//             <ul>
+//         {randomUserDataJSON.map(userDataJSON => {
+//             <li key={userDataJSON.id}>{userDataJSON.title}</li>
+//         })}
+//     </ul>
+//     </div>
+
+//   )
+// }
+
 
 export default Posts
